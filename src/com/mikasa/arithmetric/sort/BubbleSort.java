850410c2967a296e6d8bd6b1b7496ba78228f6d1
mikasa007 <17863109506@163.com>
2019-03-28 12:49:37 +0800

    冒泡排序和桶排序的比较，桶排序：速度快，但是需要大量的空间去定义桶空间，复杂度为O(n)。冒泡排序：占用空间少，但是速度慢，复杂度为O(n^2)。

---
+++ src/com/mikasa/arithmetric/sort/BubbleSort.java
@@ -0,0 +1,48 @@
+package com.mikasa.arithmetric.sort;
+
+/**
+ * @ClassName BubbleSort
+ * @description 冒泡排序
+ * 基本思想是：每次比较相邻的两个元素，如果顺序不对，则交换这两个数
+ * 优点：占用空间少  不足:速度慢
+ * @author: tianluhua
+ * @date 2019/3/28 12:13
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int[] bigData=new int[100000];
+        for (int i =99999; i >0 ; i--) {
+            bigData[i]=i;
+        }
+        long l = System.currentTimeMillis();
+        int[] ints = testBubbleSort(bigData);
+        long l1 = System.currentTimeMillis();
+        System.out.println(l1-l);
+        for (int anInt : ints) {
+            System.out.print(anInt + "  ");
+        }
+    }
+
+    /**
+     * 从小到大进行排序
+     *
+     * @param dataArray
+     * @return int[]
+     * @date 2019/3/28 12:16
+     */
+    public static int[] testBubbleSort(int[] dataArray) {
+
+        int tempData = 0;
+        for (int i = 0; i < dataArray.length - 1; i++) {
+            for (int j = i + 1; j < dataArray.length; j++) {
+                if (dataArray[i] > dataArray[j]) {
+                    tempData = dataArray[i];
+                    dataArray[i] = dataArray[j];
+                    dataArray[j] = tempData;
+                }
+            }
+        }
+        return dataArray;
+    }
+}
